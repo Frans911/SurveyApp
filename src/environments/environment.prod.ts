e1c97785a4c551610cd8c7cc3f5cece02122ebea
Frans911 <212281972@tut4life.ac.za>
2020-01-24 14:33:19 +0200

    Third Commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyD7BoCGxGNVyhaZmSZApC_mfIk4qcQptCQ",
+    authDomain: "surveyapp-e52d3.firebaseapp.com",
+    databaseURL: "https://surveyapp-e52d3.firebaseio.com",
+    projectId: "surveyapp-e52d3",
+    storageBucket: "surveyapp-e52d3.appspot.com",
+    messagingSenderId: "1060886146852",
+    appId: "1:1060886146852:web:fc0ca1074d5acde81c0db1",
+    measurementId: "G-2439PV4H67"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig : {
     apiKey: "AIzaSyD7BoCGxGNVyhaZmSZApC_mfIk4qcQptCQ",
     authDomain: "surveyapp-e52d3.firebaseapp.com",
